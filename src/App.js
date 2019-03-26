de0e6307831c929c6b756fd0960f9e2e50a5431e
Juan <juan.chavez@autozone.com>
2019-03-26 09:41:05 -0700

    building a search feature

--- src/App.js
+++ src/App.js
@@ -9,24 +9,26 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     };
   }
 
   componentDidMount() {
-    
+    this.performSearch();
   }
 
-  performSearch = (query) => {
+  performSearch = (query = 'cats') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
-    .then(response => {
+      .then(response => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
+          loading: false
         });
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
   }
 
   render() {
@@ -36,11 +38,11 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch}/>
+            <SearchForm onSearch={this.performSearch} />
           </div>
         </div>
         <div className="main-content">
-          <GifList data={this.state.gifs}/>
+          {(this.state.loading) ? <p>Loading</p> : <GifList data={this.state.gifs} />}
         </div>
       </div>
     );
